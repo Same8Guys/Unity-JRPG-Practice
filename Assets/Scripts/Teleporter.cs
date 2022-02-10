9e95f8ec8388a9bdef66511a6d69aed62a4ade0d
Shino Stott <stottshino@gmail.com>
2022-02-10 20:08:33 +0000

    teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[DisallowMultipleComponent]
+public class Teleporter : MonoBehaviour
+{
+    private Collider2D sensor;
+    public string destinationScene;
+    public float x, y;
+    public string destinationLayer;
+
+    void Awake()
+    {
+        sensor = GetComponent<Collider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
